432c9a40eaf145beb4aaacd6af0156205ef8f588
Neavy18 <genevieve.gt18@gmail.com>
2021-12-08 13:28:06 -0500

    Clean up version of previous files with added comments

--- express_server.js
+++ express_server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -6,7 +5,6 @@ const bodyParser = require("body-parser");
 const cookieParser =require('cookie-parser')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -14,28 +12,39 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+//login 
 app.post("/login", (req, res) => {
   let username = req.body.username
   res.cookie("username", username)
   res.redirect("/urls")
 })
 
+//logout 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls")
+})
+
+//match short URL and long URL
+app.post("/urls", (req, res) => {
+  let shortRanURL = generateRandomString();
+  urlDatabase[shortRanURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+//delete URL 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
 });
 
+//edit URL 
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.newURL
   res.redirect("/urls")
 });
 
-app.post("/urls", (req, res) => {
-  let shortRanURL = generateRandomString();
-  urlDatabase[shortRanURL] = req.body.longURL;
-  res.redirect(`/urls/${shortRanURL}`);
-});
-
+//my URLS page
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
@@ -44,6 +53,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//create TinyURL page
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"]
@@ -51,6 +61,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+// reassign shortURl to other longURL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL, 
@@ -60,18 +71,8 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
+//if short URL is assigned to valid longURl, redirects to page
+//if not, returns error message
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   if (!longURL) {
@@ -80,6 +81,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//generates random string for short URL
 const generateRandomString = () => {
   
   let random = [];
@@ -95,3 +97,15 @@ const generateRandomString = () => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
